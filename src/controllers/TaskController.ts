4364abaf99dfd927352a865557d0569ded3cf0f6
MarcosRios2000 <marcosrioos@gmail.com>
2024-08-24 09:36:43 -0300

    feat: delete task

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -88,4 +88,22 @@ export class TaskController {
             res.status(500).json({error: 'Hubo un error'})
         }
     }
+
+    
+    static deleteTask = async (req: Request, res: Response) => {   
+        try {
+            const { taskId } = req.params
+            const task = await Task.findById(taskId, req.body)
+            if(!task){
+                const error = new Error('Tarea no encontrada')
+                return res.status(400).json({error: error.message})
+            }
+            req.project.tasks = req.project.tasks.filter( task => task.toString() !== taskId )
+
+            await Promise.allSettled([ task.deleteOne(), req.project.save() ])
+            res.send('Tarea eliminada correctamente')
+        } catch (error) {
+            res.status(500).json({error: 'Hubo un error'})
+        }
+    }
 }
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -78,5 +78,10 @@ router.put('/:projectId/tasks/:taskId',
     TaskController.updateTask
 )
 
+router.delete('/:projectId/tasks/:taskId',
+    param('taskId').isMongoId().withMessage('ID no valido'),
+    handleInputErrors,
+    TaskController.deleteTask
+)
 
 export default router
