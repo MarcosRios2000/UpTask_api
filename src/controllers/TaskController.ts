fe7d3ea5f51b4521138fa0d1bd3d825924646fb3
MarcosRios2000 <marcosrioos@gmail.com>
2024-08-24 09:42:31 -0300

    fix: update task now use findById

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -74,7 +74,7 @@ export class TaskController {
     static updateTask = async (req: Request, res: Response) => {   
         try {
             const { taskId } = req.params
-            const task = await Task.findByIdAndUpdate(taskId, req.body)
+            const task = await Task.findById(taskId)
             if(!task){
                 const error = new Error('Tarea no encontrada')
                 return res.status(400).json({error: error.message})
@@ -83,6 +83,10 @@ export class TaskController {
                 const error = new Error('Acción no válida')
                 return res.status(404).json({error: error.message})
             }
+
+            task.name = req.body.name
+            task.description = req.body.description
+            await task.save()
             res.send('Tarea actualizada correctamente')
         } catch (error) {
             res.status(500).json({error: 'Hubo un error'})
