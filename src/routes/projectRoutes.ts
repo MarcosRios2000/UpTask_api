861236d84a7bbebb3c3caf53f112f3b4d19fa48e
MarcosRios2000 <marcosrioos@gmail.com>
2024-08-24 12:17:27 -0300

    refactor: validate task exist middleware

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -3,7 +3,8 @@ import { body, param } from 'express-validator'
 import { ProjectController } from '../controllers/ProjectController'
 import { handleInputErrors } from '../middleware/validation'
 import { TaskController } from '../controllers/TaskController'
-import { validateProjectExists } from '../middleware/project'
+import { projectExists } from '../middleware/project'
+import { taskExists } from '../middleware/task'
 
 const router = Router()
 
@@ -46,7 +47,10 @@ router.delete('/:id',
 
 
 // routes for tasks
-router.param('projectId', validateProjectExists)
+router.param('projectId', projectExists)
+
+router.param('taskId', taskExists)
+
 
 router.post('/:projectId/tasks',
     body('name')
